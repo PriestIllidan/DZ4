abd1b576b8acf7398dec79ab22d16ee6c251e49e
Александр Чичков <pristnadal@gmail.com>
2022-10-07 11:37:20 +0300

    task1.1

--- task1.py
+++ task1.py
@@ -3,12 +3,13 @@
 
 def simple_mult(n):
     lst = []
-    for i in range(1, n+1):
-        if n%i == 0:
+    for i in range(2, n+1):
+        while n%i == 0:
             lst.append(i)
+            n= n/i
     return lst
 
 N = int(input("Введите натуральное число N: "))
 lst = simple_mult(N)
-print(lst)
+print("Простыми множителями числа", N, "являются", lst)
 
